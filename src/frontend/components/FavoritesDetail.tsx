fee70a84b9310e0f6a3d12739e611398af494f21
gabman8 <gabrybenyx58@gmail.com>
2025-11-15 20:02:25 +0100

    git config --global user.name "John Doe"

--- src/frontend/components/FavoritesDetail.tsx
+++ src/frontend/components/FavoritesDetail.tsx
@@ -1,7 +1,26 @@
 
 
-export default function FavoritesDetail() {
-  return (
-    <div>FavoritesDetail</div>
-  )
+export default  function FavoritesItem({favorite, setFavorite}){
+    
+return (
+    <div>    
+        <div>
+            <p>
+                { favorite.Brand  }  
+            </p>
+            <p>
+                { favorite.Model  }  
+            </p>
+
+            <div>
+                <img src="https://image.com"></img>
+            </div>
+            <button onClick={()=> setFavorite(favorite) }></button>
+
+        </div>
+        
+    </div>
+
+)
+
 }
---
+++ src/frontend/components/FavoritesList.tsx
@@ -0,0 +1,13 @@
+export default  function FavoritesItem({favorite, setFavorite}){
+    
+return (
+    <div>    
+        { Favorites.forEach(favorite => {
+            
+        });
+        }
+    </div>
+
+)
+
+}
